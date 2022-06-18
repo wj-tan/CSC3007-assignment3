66c59dc49f5ad0a847f9514023bd042312bf4c50
wj-tan <60432453+wj-tan@users.noreply.github.com>
2022-06-18 13:32:31 +0800

    Added mouseover tooltip for population

--- script.js
+++ script.js
@@ -9,6 +9,23 @@ Promise.all([d3.json("sgmap.json"), d3.csv("population2021.csv")]).then(
     console.log(data[0]);
     console.log(data[1]);
 
+    // Setting up population data
+    populationData = [];
+    data[0].features.forEach((element) => {
+      // console.log(element)
+      data[1].forEach((element2) => {
+        // console.log(element2)
+        if (
+          element.properties.Name.toLowerCase() ==
+          element2.Subzone.toLowerCase()
+        ) {
+          populationData[element.properties.Name] = element2.Population;
+        }
+      });
+    });
+
+    console.log(populationData);
+
     // Map and projection
     var projection = d3
       .geoMercator()
@@ -31,6 +48,24 @@ Promise.all([d3.json("sgmap.json"), d3.csv("population2021.csv")]).then(
       .enter()
       .append("path")
       .attr("d", geopath)
-      .attr("fill", "black");
+      .attr("fill", "black")
+      .on("mouseover", (event, d) => { 
+        d3.select(".tooltip").html(
+          "<h4>" +
+            d.properties.Name +
+            "</h4>" +
+            "Population: " +
+            populationData[d.properties.Name.toUpperCase()]
+        );
+
+        let path = d3.select(event.currentTarget);
+        path.style("stroke", "red").style("stroke-width", 2);
+      })
+      .on("mouseout", (event, d) => { 
+        d3.select(".tooltip").text("");
+
+        let path = d3.select(event.currentTarget);
+        path.style("stroke", "none");
+      });
   }
 );
